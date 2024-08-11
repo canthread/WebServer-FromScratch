60f9a31f4e51cf4d69379587854753985432fe25
michael-torkaman <michael.torkaman.t@gmail.com>
2024-08-10 23:48:30 -0700

    error in binding to port

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d758edc409613d0e408024aee4b70d31800108ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3434601102d0ef266656657d5cd71442c8f2452a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
